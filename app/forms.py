cbede38ff4084e05ac95a5e0f8de0d3858cd8031
AlkaffAhamed <alkaffahamed@hotmail.com>
2021-09-09 00:02:36 +0800

    Reformated Code & Update Readme - Fixed inconsistent tab and 4 space indents => all indents 4 spaces - PyCharm built-in reformat to match PEP 8 standard - Update Readme.md: Added project documentation contents

--- app/forms.py
+++ app/forms.py
@@ -50,28 +50,32 @@ class AddVehicleForm(FlaskForm):
     odometer = DecimalField('Odometer', validators=[DataRequired()])
     submit = SubmitField('Add Vehicle')
 
+
 class TopUpForm(FlaskForm):
-	value = DecimalField('Top Up Value', validators= [NumberRange(min=0, max=100, message='bla')] )
-	submit = SubmitField('Top Up')
+    value = DecimalField('Top Up Value', validators=[NumberRange(min=0, max=100, message='bla')])
+    submit = SubmitField('Top Up')
+
 
 class AddAdminForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	submit = SubmitField('Add admin')
-	
-	def validate_username(self, username):
-		user = User.query.filter_by(username=username.data).first()
-		if user is not None:
-			raise ValidationError('Please use a different username.')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Add admin')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
 
 
 class AddVehicleForm(FlaskForm):
-	vehicleType = RadioField('Vehicle Type', choices = [('Car', 'Car'),('Lorry','Lorry'),('Van','Van')], validators=[DataRequired()])
-	vehicleNum = StringField('Vehicle Number', validators=[DataRequired()])
-	modelNumber = StringField('Model Number', validators=[DataRequired()])
-	# purchaseDate = DateField('Purchase Date', validators=[DataRequired()]) 
-	odometer = DecimalField('Odometer', validators= [DataRequired()] )
-	submit = SubmitField('Add Vehicle')
+    vehicleType = RadioField('Vehicle Type', choices=[('Car', 'Car'), ('Lorry', 'Lorry'), ('Van', 'Van')],
+                             validators=[DataRequired()])
+    vehicleNum = StringField('Vehicle Number', validators=[DataRequired()])
+    modelNumber = StringField('Model Number', validators=[DataRequired()])
+    # purchaseDate = DateField('Purchase Date', validators=[DataRequired()])
+    odometer = DecimalField('Odometer', validators=[DataRequired()])
+    submit = SubmitField('Add Vehicle')
+
 
 class GetAvailableVehicles(FlaskForm):
     vehicleType = RadioField('Vehicle Type', choices=[('Car', 'Car'), ('Lorry', 'Lorry'), ('Van', 'Van')],
@@ -81,7 +85,7 @@ class GetAvailableVehicles(FlaskForm):
     submit = SubmitField("Search")
 
 
-#ref: https://stackoverflow.com/questions/59554877/flask-form-with-parameters
+# ref: https://stackoverflow.com/questions/59554877/flask-form-with-parameters
 class BookVehicle(FlaskForm):
     vehicleNum = SelectField("Vehicle Number", choices=[], validators=[DataRequired()])
     submit = SubmitField("Book!")
@@ -93,6 +97,7 @@ class BookVehicle(FlaskForm):
     #     print(self.choiceslist)
     #     print(self.vehicleNum)
     #     print(self.submit)
+
 
 class SelectRentOutVehicle(FlaskForm):
     vehicleNum = SelectField("Vehicle Number", choices=[], validators=[DataRequired()])
--- app/models.py
+++ app/models.py
@@ -26,84 +26,90 @@ def load_user(id):
 
 
 class User(UserMixin, db.Model):
-	__tablename__ = 'user'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
-	password_hash = db.Column(db.String(128))
-	type = db.Column(db.String)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    type = db.Column(db.String)
 
-	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-	def check_password(self, password):
-		return check_password_hash(self.password_hash, password)
-	
-	def __repr__(self):
-		return f'<User {self.username:}>'
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username:}>'
+
 
 class Customer(User):
-	balance = db.Column(db.Float(64))
-	transaction = db.relationship('Transaction', backref='customer',lazy='dynamic')
-	
-	def __init__(self, username):
-		self.balance = 0
-		self.type = 'customer'
-		self.username = username
+    balance = db.Column(db.Float(64))
+    transaction = db.relationship('Transaction', backref='customer', lazy='dynamic')
 
-	def check_balance(self):
-		return self.balance
-	
-	def deduct_balance(self, value):
-		self.balance = self.balance - value
-		return self.balance
-	
-	def add_balance(self, value):
-		self.balance = self.balance + value
-		return self.balance
-	
+    def __init__(self, username):
+        self.balance = 0
+        self.type = 'customer'
+        self.username = username
+
+    def check_balance(self):
+        return self.balance
+
+    def deduct_balance(self, value):
+        self.balance = self.balance - value
+        return self.balance
+
+    def add_balance(self, value):
+        self.balance = self.balance + value
+        return self.balance
+
+
 class AdminUser(User):
-	def __init__(self, username):
-		self.type = 'admin'
-		self.username=username
+    def __init__(self, username):
+        self.type = 'admin'
+        self.username = username
 
-	def add_admin_user(self, user_details):
-		username = user_details[0]
-		password = user_details[1]
-		new_admin = AdminUser(username=username)
-		new_admin.set_password(password)
-		return new_admin
+    def add_admin_user(self, user_details):
+        username = user_details[0]
+        password = user_details[1]
+        new_admin = AdminUser(username=username)
+        new_admin.set_password(password)
+        return new_admin
+
 
 class Vehicle(db.Model):
-	__tablename__ = 'vehicle'
-	id = db.Column(db.Integer, primary_key=True)
-	vehicle_num = db.Column(db.String(64))
-	model_number = db.Column(db.String(64))
-	# purchase_date = db.Column(db.String(64))
-	odometer = db.Column(db.Float(64))
-	vehicle_type = db.Column(db.String(64))
-	unit_price = db.Column(db.Float(64))
-	transaction = db.relationship('Transaction', backref='vehicle', lazy='dynamic')
-	# update odo
+    __tablename__ = 'vehicle'
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_num = db.Column(db.String(64))
+    model_number = db.Column(db.String(64))
+    # purchase_date = db.Column(db.String(64))
+    odometer = db.Column(db.Float(64))
+    vehicle_type = db.Column(db.String(64))
+    unit_price = db.Column(db.Float(64))
+    transaction = db.relationship('Transaction', backref='vehicle', lazy='dynamic')
 
-	def update_odo(self, new_odo):
-		self.odometer = new_odo
-		
+    # update odo
+
+    def update_odo(self, new_odo):
+        self.odometer = new_odo
+
 
 class Car(Vehicle):
-	def __init__(self):
-		self.unit_price = 90.0
-		self.vehicle_type = 'Car'
+    def __init__(self):
+        self.unit_price = 90.0
+        self.vehicle_type = 'Car'
+
 
 class Van(Vehicle):
-	def __init__(self):
-		self.unit_price = 100.0
-		self.vehicle_type = 'Van'
+    def __init__(self):
+        self.unit_price = 100.0
+        self.vehicle_type = 'Van'
+
 
 class Lorry(Vehicle):
-	def __init__(self):
-		self.unit_price = 120.0
-		self.vehicle_type = 'Lorry'
-	
+    def __init__(self):
+        self.unit_price = 120.0
+        self.vehicle_type = 'Lorry'
+
 
 class Transaction(db.Model):
     __tablename__ = 'transactions'
