ea8308ee36ef001e744371aae7a07440d94b3fb4
AlkaffAhamed <alkaffahamed@hotmail.com>
2021-09-09 04:33:30 +0800

    Comleted Transaction Mechanism - Added relevant pages for relevant state of transaction    - select_vehicle       - Query and select vehicle    - book_vehicle       - Create transaction with state = "Booked"    - select_paytransaction       - state = "Paid" upon successful payment       - Redirect to topup page if payment fails    - select_rentout       - state = "Dispatch"    - select_return       - state = "Complete"

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ import app.models
 from app import application
 from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm, RegistrationForm, TopUpForm, AddAdminForm, AddVehicleForm, GetAvailableVehicles, \
-    BookVehicle
+    BookVehicle, SelectPayTransaction, SelectRentOutVehicle, SelectReturnVehicle
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Customer, AdminUser, Car, Van, Lorry, Vehicle, Transaction
 from werkzeug.urls import url_parse
@@ -240,7 +240,7 @@ def selectVehicle():
             .filter(db.and_(db.or_(Transaction.start_date.between(startDate, endDate),
                                    (Transaction.start_date + Transaction.book_duration)
                                    .between(startDate, endDate)),
-                            Transaction.state.in_(["Booked", "Dispatch"]))) \
+                            Transaction.state.in_(["Booked", "Dispatch", "Paid"]))) \
             .all()
 
         rejectedVid = [t.vid for t in qry2]
@@ -294,8 +294,9 @@ def bookVehicle():
                         amount=selectedVehicle.unit_price * bookdays)
         db.session.add(r)
         db.session.commit()
+        flash(f"You have booked {selectedVehicle.vehicle_num} from {startDate} to {endDate}.")
 
-        return redirect(url_for("index"))
+        return redirect(url_for("selectPayTransaction"))
 
     return render_template("book_vehicle.html", startDate=startDate,
                            endDate=endDate, vehicles=vehicles, bookingForm=form)
@@ -325,30 +326,133 @@ def bookVehicle():
 #     return render_template("book_vehicle.html", startDate=startDate,
 #                            endDate=endDate, vehicles=vehicles, bookingForm=bookForm)
 
+@application.route('/selectpaytransaction', methods=["GET", "POST"])
+@login_required
+def selectPayTransaction():
+    qry1 = db.session.query(Transaction, Vehicle) \
+        .join(Vehicle, Transaction.vid == Vehicle.id) \
+        .filter(Transaction.cid == current_user.id) \
+        .filter(Transaction.state == "Booked") \
+        .all()
+
+    print(qry1)
+    print([(t[0].id, t[1].id, t[1].vehicle_num, t[0].start_date, t[0].end_date) for t in qry1])
+    choices = [(str(t[0].id), str(t[0].id)) for t in qry1]
+
+    form = SelectPayTransaction()
+    form.tid.choices = choices
+
+    if form.validate_on_submit():
+        tid = int(form.tid.data)
+        # print(form.tid.data)
+        qry2 = db.session.query(Transaction).filter(Transaction.id == tid).first()
+        print(qry2, type(qry2), "$", qry2.amount)
+
+        customer = db.session.query(Customer).filter(Customer.id == current_user.id).first()
+        print(customer, type(customer), "$", customer.check_balance())
+
+        # Check if user has enough money
+        if customer.check_balance() < qry2.amount:
+            print("INSUFFICIENT FUNDS")
+            flash("Insufficient funds in your wallet. Please top up.")
+            return redirect(url_for("balance"))
+        else:
+            amt = qry2.amount
+            customer.deduct_balance(float(amt))
+            qry2.state = "Paid"
+            db.session.commit()
+            flash("Successfully Paid!")
+            return render_template("select_paytransaction.html", transvehicle=qry1, form=form)
+
+    return render_template("select_paytransaction.html", transvehicle=qry1, form=form)
+
+
+# @application.route('/paytransaction', methods=["GET", "POST"])
+# @login_required
+# def payTransaction():
+#     pass
+
 
 @application.route('/selectrentout', methods=["GET", "POST"])
 @login_required
 def selectRentOutVehicle():
-    # State = Dispatch
-    pass
+    qry1 = db.session.query(Transaction, Vehicle) \
+        .join(Vehicle, Transaction.vid == Vehicle.id) \
+        .filter(Transaction.cid == current_user.id) \
+        .filter(Transaction.state == "Paid") \
+        .all()
 
+    print(qry1)
+    print([(t[0].id, t[1].id, t[1].vehicle_num, t[0].start_date, t[0].end_date) for t in qry1])
+    choices = [(str(t[0].id), t[1].vehicle_num) for t in qry1]
 
-@application.route('/rentout', methods=["GET", "POST"])
-@login_required
-def rentOutVehicle():
-    # State = Dispatch
-    pass
+    form = SelectRentOutVehicle()
+    form.vehicleNum.choices = choices
+    print(form.validate_on_submit())
+
+    if form.validate_on_submit():
+        tid = int(form.vehicleNum.data)
+        print("ID", type(tid), tid)
+        vl = [t for t in qry1 if t[0].id == tid]
+        print("VL", vl)
+        selectedVehicle = vl[0]
+        print("NOLIST", selectedVehicle)
+
+        qry2 = db.session.query(Transaction).filter(Transaction.id == selectedVehicle[0].id).first()
+        print(qry2, type(qry2), "$", qry2.state)
+        qry2.state = "Dispatch"
+        qry2.start_odo = selectedVehicle[1].odometer
+
+        db.session.commit()
+        flash("Successfully dispatched your vehicle!")
+        return redirect(url_for("index"))
+    return render_template("select_rentout.html", transvehicle=qry1, form=form)
+
 
 
 @application.route('/selectreturn', methods=["GET", "POST"])
 @login_required
 def selectReturnVehicle():
-    # State = Paid
-    pass
+    qry1 = db.session.query(Transaction, Vehicle) \
+        .join(Vehicle, Transaction.vid == Vehicle.id) \
+        .filter(Transaction.cid == current_user.id) \
+        .filter(Transaction.state == "Dispatch") \
+        .all()
+
+    print(qry1)
+    print([(t[0].id, t[1].id, t[1].vehicle_num, t[0].start_date, t[0].end_date) for t in qry1])
+    choices = [(str(t[0].id), t[1].vehicle_num) for t in qry1]
+
+    form = SelectReturnVehicle()
+    form.vehicleNum.choices = choices
+    print(form.validate_on_submit())
+
+    if form.validate_on_submit():
+        tid = int(form.vehicleNum.data)
+        odo = form.odo.data
+        dateReturn = form.datereturn.data
+        print("ID ODO DATE", type(tid), tid, type(odo), odo, type(dateReturn), dateReturn)
+        vl = [t for t in qry1 if t[0].id == tid]
+        print("VL", vl)
+        selectedVehicle = vl[0]
+        print("NOLIST", selectedVehicle)
+
+        qry2 = db.session.query(Transaction).filter(Transaction.id == selectedVehicle[0].id).first()
+        print(qry2, type(qry2), qry2.state)
+        qry2.state = "Complete"
+        qry2.end_date = dateReturn
+        qry2.end_odo = odo
+        print(qry2, type(qry2), qry2.state, qry2.end_odo)
+
+        qry3 = db.session.query(Vehicle).filter(Vehicle.id == selectedVehicle[1].id).first()
+        print(qry3, type(qry3), qry3.odometer)
+        qry3.update_odo(odo)
+        print(qry3, type(qry3), qry3.odometer)
+
+        db.session.commit()
+        flash("Successfully returned your vehicle!")
+        return redirect(url_for("index"))
+
+    return render_template("select_return.html", transvehicle=qry1, form=form)
 
 
-@application.route('/return', methods=["GET", "POST"])
-@login_required
-def returnVehicle():
-    # State = Paid
-    pass
